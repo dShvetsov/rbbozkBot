c42f9261cc578bbcd24b1ebb54e9235bdbf0ee3f
dshvetsov <dshvetsov@arccn.ru>
2016-11-01 19:04:50 +0300

    v1

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 from random import randint
+import urllib3
 import telebot
 import json
 from mytoken import TOKEN, password
@@ -29,14 +30,14 @@ def getUnread():
 
 @bot.message_handler(content_types=["text"])
 def handler(message):
-   user_id = message.chat.id
-   global  state, ret, users, d
-   if not user_id in users.keys():
+    user_id = message.chat.id
+    global  state, ret, users, d
+    if not user_id in users.keys():
         users[user_id] = "untrusted"
         bot.send_message(user_id, "Hello,  please input password")
         print ("new users detected, waiting password")
         return
-   elif users[user_id] == 'untrusted' :
+    elif users[user_id] == 'untrusted' :
         if check(message.text) :
             users[user_id] = 'trusted'
             bot.send_message(user_id, "Welcome")
@@ -46,25 +47,52 @@ def handler(message):
             bot.send_message(user_id, "Wrong password, try again")
             print ("wrong password")
             return
-   elif users[user_id] != 'trusted' :
+    elif users[user_id] != 'trusted' :
         print ("error")
         return
 
-   if (message.text == 'get') :
+    if (message.text == 'get') :
         ret = getUnread()
         print (ret)
         state = "expect result"
         bot.send_message(user_id, ret)
-   elif state == "expect result" :
-        if message.text == 'done' : 
+    elif state == "expect result" :
+        if message.text == 'done' :
             d[ret] = 'yes'
             print (ret + " done")
             bot.send_message(user_id, "ok, you read this message")
 
-   if message.text == 'dump' : 
+    if message.text == 'dump' :
         json.dump(d, open('./dict.dmp', 'w'))
         print ("dumped")
         bot.send_message(user_id, "file dumped")
 
+    if message.text == 'update' :
+        update()
+        print ("updated")
+        bot.send_message(user_id, "new article retrivied")
+
+def update():
+    global d
+    patt = re.compile(r'href=[\'"]?([^\'" >]+)')
+    pagename = "https://habrahabr.ru/users/rbbozk/favorites/page"
+    i = 0
+    http = urllib3.PoolManager()
+    while (True) :
+        i += 1
+        response = http.request(pagemane + str(i))
+        if reponse != 200 :
+            return
+        html = reponse.read().decode('utf-8')
+        lines = html.splitlines()
+        lines = filter(lambda x : x.find('class="post__title_link"') != -1, lines)
+        #lines = [i for i in lines]
+        for l in lines :
+            link = pattern.search(l)
+            if link in d :
+                return
+            else  :
+                d[link] = 'no'
+
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+     bot.polling(none_stop=True)
